310c9a8d9f566bd17f4d84329e185b523fc7d008
ArseniyK1 <qwertototo2332@gmail.com>
2023-09-28 23:11:54 +0300

    backup post-ulbi-tv

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import componets from "@/components/UI";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 
 componets.forEach((component) => app.component(component.name, component));
 
-app.mount("#app");
+app.use(createPinia()).mount("#app");
